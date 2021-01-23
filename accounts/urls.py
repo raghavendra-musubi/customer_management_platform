ca33f4bde42f5f936ad190be9730a236b3507703
raghavendra-musubi <raghavendra@techis.io>
2021-01-24 02:12:04 +0530

    end of video 10

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     # path('about/', views.about, name='about'),
     path('customer/<str:pk>', views.customer, name='customer'),
     path('products/', views.products, name='products'),
+    path('order/', views.order_create, name='order_create'),
+    path('order/update/<str:pk>/', views.order_update, name='order_update'),
+    path('order/delete/<str:pk>/', views.order_delete, name='order_delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -32,3 +33,48 @@ def customer(request, pk):
     }
 
     return render(request, 'accounts/customer.html', context)
+
+
+def order_create(request):
+
+    form = OrderForm
+    if request.method == 'POST':
+        # print('#', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/order_form.html', context )
+
+def order_update(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/order_form.html', context )
+
+def order_delete(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+ 
+    context = {
+        'order': order
+    }
+
+    return render(request, 'accounts/order_delete.html', context )
